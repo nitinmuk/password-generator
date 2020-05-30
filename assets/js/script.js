bd24b0b87d1b569d37a5f94f273a21bb1bcce836
Nitin Mukesh <nitinmukes@gmail.com>
2020-05-30 17:11:51 +0800

    adding semicolon to be consistent across file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function writePassword()
 {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
